4621505ca8bfc0c0deede914d9476dce825cee3f
Buriev Sardor <your@email.com>
2023-07-26 16:03:41 +0500

    encrypt definition moved from Message.h to Message.cpp

--- message.cpp
+++ message.cpp
@@ -1,3 +1,16 @@
 #include "message.h"
 
 Message::Message(const QString& str) : _str(str) { }
+
+QString Message::encrypt(){
+
+    auto temp = _str.toStdString();
+
+    for(std::size_t i = 0; i < temp.size(); i++){
+
+        temp[i] = temp[i] + 1;
+    }
+
+    temp = "HELLO"+temp;
+    return QString::fromStdString(temp);
+}
--- message.h
+++ message.h
@@ -8,18 +8,7 @@ class Message
 public:
     explicit Message(const QString& str);
 
-    QString encrypt(){
-
-        auto temp = _str.toStdString();
-
-        for(std::size_t i = 0; i < temp.size(); i++){
-
-            temp[i] = temp[i] + 1;
-        }
-
-        temp = "HELLO"+temp;
-        return QString::fromStdString(temp);
-    }
+    QString encrypt();
 
 private:
     QString _str;
